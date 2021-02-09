ecde215c74ed9eacc3622db92939fa6296b7d17e
Pablo Woolvett <pablowoolvett@gmail.com>
2021-02-09 20:25:36 +0000

    feat(cli): added flakehell entrypoints
    Signed-off-by: Pablo Woolvett <pablowoolvett@gmail.com>

--- src/drytoml/cli.py
+++ src/drytoml/cli.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 """"""
 import importlib
+import os
 import sys
 import tempfile
 from contextlib import contextmanager
-
+import shutil
 from drytoml.parser import Parser
 
-PROVIDERS = {
+CLI_PROVIDERS = {
     "black": ("black:patched_main", ["--config"]),
     "isort": (
         "isort.main:main",
         ["--sp", "--settings-path", "--settings-file", "--settings"],
     ),
+}
+
+ENVIRON_PROVIDERS = {
+    "flake8helled": ("flakehell:flake8_entrypoint", "FLAKEHELL_TOML"),
+    "flakehell": ("flakehell:entrypoint", "FLAKEHELL_TOML"),
 }
 
 
@@ -23,7 +29,7 @@ def tmp_dump(cfg: str):
     with tempfile.NamedTemporaryFile(
         mode="w+",
         suffix=".toml",
-        prefix="drytoml.black",
+        prefix="drytoml.",
     ) as fp:
         fp.write(document.as_string())
         fp.seek(0)
@@ -37,7 +43,17 @@ def import_callable(string):
     return tool_main
 
 
-def impl(importstr, configs):
+def impl_env(importstr, env):
+
+    cfg = os.environ.get(env, "pyproject.toml")
+
+    with tmp_dump(cfg) as virtual:
+        os.environ[env] = virtual.name
+        # NOTE: import should go after env definition just to be safe
+        tool_main = import_callable(importstr)
+        sys.exit(tool_main())
+
+def impl_cli(importstr, configs):
     tool_main = import_callable(importstr)
     for option in configs:
         try:
@@ -55,31 +71,47 @@ def impl(importstr, configs):
 
     with tmp_dump(cfg) as virtual:
         sys.argv = [*pre, option, f"{virtual.name}", *post]
+
         sys.exit(tool_main())
 
 
-def black():
-    from black import patched_main
+def clear_cache():
+    from drytoml.paths import CACHE
+    truthy = {'t', "true", "y","yes", "clear"}
+    response = input("Clear cache? [y/N]")
+    if response.lower() not in truthy:
+        print("Aborted")
+        return
 
-    try:
-        idx = sys.argv.index("--config")
-        pre = sys.argv[:idx]
-        post = sys.argv[idx + 2 :]
-        cfg = sys.argv[idx + 1]
-    except ValueError:
-        pre = sys.argv
-        post = []
-        cfg = "pyproject.toml"
+    for cache in CACHE.glob("**/*"):
+        if cache.is_file():
+            cache.unlink()
+        elif cache.is_dir():
+            shutil.rmtree(str(cache.resolve()))
 
-    with tmp_dump(cfg) as virtual:
-        sys.argv = [*pre, "--config", f"{virtual.name}", *post]
-        patched_main()
+
+INTERNAL = {
+    "cache-clear": clear_cache,
+}
 
 def main():
     del sys.argv[0]
     provider = sys.argv[0]
-    return impl(*PROVIDERS[provider])
+
+    if provider in CLI_PROVIDERS:
+        args = CLI_PROVIDERS[provider]
+        return impl_cli(*args)
+
+    if provider in ENVIRON_PROVIDERS:
+        args = ENVIRON_PROVIDERS[provider]
+        return impl_env(*args)
+
+    if provider in INTERNAL:
+        return INTERNAL[provider]()
+
+    print(f"Invalid command {provider}")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
-    black()
+    main()
